4f7bb1801f7d9f43bd2d53ce854aa26b8a146316
nixeneko <nixeneko@users.noreply.github.com>
2021-06-08 23:43:19 +0900

    Ignores non-existing temperature file

--- ds18b20.py
+++ ds18b20.py
@@ -1,11 +1,15 @@
 # coding: utf-8
 # あ
 import subprocess
+import os
 
 def get_func_read_data(temperature_file):
     return lambda: read_data(temperature_file)
 
 def read_data(temperature_file):
+    #ignore if temperatue_file does not exist
+    if not os.path.exists(temperature_file):
+        return None
     tfiles = subprocess.check_output(['cat',temperature_file]).decode('utf-8')
     if tfiles.find("YES",30,60) != -1:
         idx = tfiles.find("t=",50,80)
