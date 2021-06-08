5391b678a47f7b6c475b05ef9ae8ad4eae09347f
nixeneko <nixeneko@users.noreply.github.com>
2021-06-08 23:20:14 +0900

    Adds multiple censor logging support

---
+++ ds18b20.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# あ
+import subprocess
+
+def get_func_read_data(temperature_file):
+    return lambda: get_data(temperature_file)
+
+def read_data(temperature_file):
+    tfiles = subprocess.check_output(['cat',temperature_file]).decode('utf-8')
+    if tfiles.find("YES",30,60) != -1:
+        idx = tfiles.find("t=",50,80)
+        temperature = int(tfiles[idx+2:])/1000.0
+        return (temperature, )
--- rec_temp.py
+++ rec_temp.py
@@ -6,25 +6,27 @@ import datetime
 import os
 
 import settings
-temperature_file = settings.TEMPERATURE_FILE
-temp_log_dir = settings.TEMP_LOG_DIR
-#TEMPERATURE_FILE = "/sys/bus/w1/devices/28-xxxxxxxxxxxxxx/w1_slave"
-#TEMP_LOG_DIR = "temp_log"
+
+func_logdir_pairs = settings.FUNC_LOGDIR_PAIRS
+#data_get_funcs = settins.DATA_GET_FUNCS
+#data_log_dirs = settings.DATA_LOG_DIRS
+
+# change current directory to the directory this script is in
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-tfiles = subprocess.check_output(['cat',temperature_file]).decode('utf-8')
-#print(tfiles)
 now = datetime.datetime.now()
 nowstr = now.isoformat().split(".")[0]
 year = str(now.year)
-dirpath = os.path.join(temp_log_dir, year)
-if not os.path.exists(dirpath):
-    os.makedirs(dirpath)
-filename = now.strftime("%Y-%m-%d") + ".tsv"
-filepath = os.path.join(temp_log_dir, year, filename) 
-if tfiles.find("YES",30,60) != -1:
-    idx = tfiles.find("t=",50,80)
-    temperature = int(tfiles[idx+2:])/1000.0
+for func, logdir in func_logdir_pairs:
+    dirpath = os.path.join(logdir, year)
+    data = func()
+    if data is None:
+        continue
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+    filename = now.strftime("%Y-%m-%d") + ".tsv"
+    filepath = os.path.join(logdir, year, filename) 
+    
     with open(filepath, "a") as w:
         w.write(nowstr + "\t" + str(temperature) + "\n")
 
